9ce8d7e464a972927c7b31c319ac5205d5c8a6cb
Craig Bourne <craigbourne15@gmail.com>
2024-10-17 19:44:05 +0100

    fixed index error

--- Overflow.py
+++ Overflow.py
@@ -1,7 +1,6 @@
 """This demonstrates a buffer overflow attempt in Python."""
 
-buffer=[None]*10
-for i in range(0,14):
-    buffer[i]=7
-
+buffer = [None] * 10
+for i in range(0, 10):
+    buffer[i] = 7
 print(buffer)
